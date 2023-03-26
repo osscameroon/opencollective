b31d61121a80afd7799afc39ab132a4019bfab6d
elhmn <elhmn@github.com>
2023-03-26 12:27:19 +0200

    billing: setup api

---
+++ billing/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/osscameroon/opencollective/billing/internal/handlers"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{},
+		AllowCredentials: true,
+	}))
+
+	//Health check endpoint
+	router.GET("/health", handlers.Health)
+
+	if err := router.Run(":7000"); err != nil {
+		return
+	}
+}
